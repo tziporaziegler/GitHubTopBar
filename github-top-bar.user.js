bb592037330ac7585dc83e28c87648b738454d68
Tzipora Ziegler <tziporaziegler@users.noreply.github.com>
2017-02-15 22:46:51 -0500

    Update github-top-bar.user.js

--- github-top-bar.user.js
+++ github-top-bar.user.js
@@ -3,9 +3,10 @@
 // @namespace   https://github.com/tziporaziegler/GitHubTopBar/
 // @description A user script for styling the GitHub top-bar
 // @author      Tzipora Ziegler
-// @include     http://github.com*
+// @include     https://github.com/*
 // @version     1.0
 // @run-at document-start
+// @require http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
@@ -24,16 +25,51 @@ function addGlobalStyle(css) {
 }
 
 //Change background to lightgrey. Reduce padding. Add grey bottom border.
-addGlobalStyle ( 
-    '.header{ background-color:#f5f5f5; padding-top:7px; padding-bottom: 7px; border-bottom:1px solid #e5e5e5; }' 
+addGlobalStyle (
+    '.header{ background-color:#f5f5f5; padding-top:7px; padding-bottom: 7px; border-bottom:1px solid #e5e5e5; }'
 );
 
 //Change nav items to black
-addGlobalStyle ( 
-    '.header-logo-invertocat .octicon-mark-github, .header-nav-link { color:black }'
+addGlobalStyle (
+    '.header-logo-invertocat .octicon-mark-github, .header-nav-link, .header .header-search-input { color:black }'
+);
+
+addGlobalStyle (
+    '.header-logo-invertocat .octicon-mark-github:hover, .header-nav-link:hover { color:#555555 }'
+);
+
+addGlobalStyle (
+    '.header-nav-link:hover .dropdown-caret, .header-nav-link:focus .dropdown-caret { border-top-color: #666666; }'
 );
 
 //Fix search bar
-addGlobalStyle ( 
+addGlobalStyle (
     '.header .header-search-wrapper { background-color:white; border: 1px solid #e5e5e5; }'
+);
+
+addGlobalStyle (
+    '.header .header-search-wrapper.focus { background-color:#fefefe; border: 1px solid #e5e5e5; }'
+);
+
+addGlobalStyle (
+    '.header .header-search-scope, .header .header-search-wrapper.focus .header-search-scope { color:#666666; border-right-color:#dddddd; }'
+);
+
+//Set the search placeholder color
+//Need a separate line for each browser because when a browser doesn’t understand a selector, it invalidates the entire line of selectors
+addGlobalStyle (
+    /* Chrome/Opera/Safari */
+    '::-webkit-input-placeholder { color:#999 !important; }'
+);
+addGlobalStyle (
+    /* Firefox 19+ */
+    '::-moz-placeholder { color:#999 !important; }'
+);
+addGlobalStyle (
+    /* IE 10+ */
+    ':-ms-input-placeholder { color:#999 !important; }'
+);
+addGlobalStyle (
+    /* Firefox 18- */
+    ':-moz-placeholder { color:#999 !important; }'
 );
