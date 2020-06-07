6c632a3a8030f8722b6cc9a67624d29088f08f41
Tzipora Ziegler <tziporaziegler@users.noreply.github.com>
2020-06-07 06:09:53 -0400

    Get username from meta tags instead of hardcoding to 'tziporaziegler' to make it easier for others to use the script.

--- github-top-bar.user.js
+++ github-top-bar.user.js
@@ -5,7 +5,7 @@
 // @author      Tzipora Ziegler
 // @include     https://github.com/*
 // @require http://code.jquery.com/jquery-3.3.1.min.js
-// @version     1.2.12
+// @version     1.3.0
 // @run-at document-start
 // @require http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
@@ -14,7 +14,7 @@ const $ = window.jQuery;
 
 (function() {
     const nameSpace = 'GitHubTopBar-';
-    const username = 'tziporaziegler';
+    const username = $('meta[name=octolytics-actor-login]').attr("content");
 
     // Create variables for easy customization
 
